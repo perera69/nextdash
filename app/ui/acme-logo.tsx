b869241c5017d30b1b6a57e92579cb11f980b970
perera69 <tharinduanjana077@gmail.com>
2024-05-07 11:45:12 +0530

    cmmit 4

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,14 +1,13 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { inter,lusitana } from '@/app/ui/fonts';
-import { Inter, Lusitana } from 'next/font/google';
+import { inter, lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.toString()} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <p className={`${inter.toString()} text-[44px]`}>Acme</p>
     </div>
   );
 }
